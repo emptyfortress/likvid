a22e7f0d8132261d09ba73b35d8501bfbc6afbdb
Volkov Alexander <volkov.525@gmail.com>
2023-02-01 10:43:51 +0300

    fix splitter

--- src/components/NewTree.vue
+++ src/components/NewTree.vue
@@ -1,13 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const ratio = ref(50)
+</script>
 
 <template lang="pug">
 q-page(padding)
 	.container
-		h2 New tree {{$route.params.id}}
+		q-splitter(v-model="ratio")
+			template(#before)
+				q-scroll-area.home
+					h2 New tree {{$route.params.id}}
+			template(#after)
+				q-scroll-area.home
+					h2 fuck
 </template>
 
 <style scoped lang="scss">
 h2 {
 	margin: 0;
+}
+.home {
+	height: calc(100vh - 99px);
 }
 </style>
