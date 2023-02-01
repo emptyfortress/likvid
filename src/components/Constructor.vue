d9431851e50a3a6722e441aecfba975bf46786c4
Volkov Alexander <volkov.525@gmail.com>
2023-02-01 12:17:55 +0300

    add tabs

--- src/components/Constructor.vue
+++ src/components/Constructor.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import PropsTab from '@/components/PropsTab.vue'
 
 const ratio = ref(30)
 </script>
 
 <template lang="pug">
 q-page(padding)
-	.container
-		q-splitter(v-model="ratio")
-			template(#before)
-				q-scroll-area.home
-					h2 New tree {{$route.params.id}}
-			template(#after)
-				q-scroll-area.home
-					h2 fuck
+	q-splitter(v-model="ratio")
+		template(#before)
+			q-scroll-area.home
+				h2 New tree {{$route.params.id}}
+		template(#after)
+			q-scroll-area.home
+				PropsTab
 </template>
 
 <style scoped lang="scss">
@@ -22,5 +22,6 @@ h2 {
 }
 .home {
 	height: calc(100vh - 99px);
+	// background: #fff;
 }
 </style>
---
+++ src/components/PropsTab.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const activeTab = ref('one')
+
+const tabs = [
+	{ id: 0, name: 'one', label: 'Общие' },
+	{ id: 1, name: 'two', label: 'Ветвь' },
+	{ id: 2, name: 'three', label: 'Представления' },
+	{ id: 4, name: 'five', label: 'Права' },
+	{ id: 3, name: 'four', label: 'Состояния' },
+]
+</script>
+
+<template lang="pug">
+q-tabs(v-model='activeTab' dense align="left").text-primary
+	q-tab(v-for='tab in tabs' :key='tab.id' :name='tab.name')
+		span {{tab.label}}
+		// 	q-badge(rounded float color="red" align="top" v-if="tab.modified")
+
+</template>
+
+<style scoped lang="scss"></style>
