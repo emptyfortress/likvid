ba81849733e0050fadc58eb08dcc0cfb521d6572
Volkov Alexander <volkov.525@gmail.com>
2023-02-01 12:22:12 +0300

    badge

--- src/components/PropsTab.vue
+++ src/components/PropsTab.vue
@@ -4,11 +4,11 @@ import { ref } from 'vue'
 const activeTab = ref('one')
 
 const tabs = [
-	{ id: 0, name: 'one', label: 'Общие' },
-	{ id: 1, name: 'two', label: 'Ветвь' },
-	{ id: 2, name: 'three', label: 'Представления' },
-	{ id: 4, name: 'five', label: 'Права' },
-	{ id: 3, name: 'four', label: 'Состояния' },
+	{ id: 0, modified: false, name: 'one', label: 'Общие' },
+	{ id: 1, modified: true, name: 'two', label: 'Ветвь' },
+	{ id: 2, modified: false, name: 'three', label: 'Представления' },
+	{ id: 4, modified: false, name: 'five', label: 'Права' },
+	{ id: 3, modified: false, name: 'four', label: 'Состояния' },
 ]
 </script>
 
@@ -16,8 +16,16 @@ const tabs = [
 q-tabs(v-model='activeTab' dense align="left").text-primary
 	q-tab(v-for='tab in tabs' :key='tab.id' :name='tab.name')
 		span {{tab.label}}
-		// 	q-badge(rounded float color="red" align="top" v-if="tab.modified")
+			q-badge(rounded float color="red" align="top" v-if="tab.modified")
 
+q-tab-panels(v-model='activeTab' animated)
+	q-tab-panel(name='one')
+		div test
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.q-badge {
+	padding: 0 4px;
+	min-height: 8px;
+}
+</style>
