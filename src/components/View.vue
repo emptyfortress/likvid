d7ebfa2f488b00734918565d1e2c947966725206
Volkov Alexander <volkov.525@gmail.com>
2023-02-10 15:31:52 +0300

    before remove tabs

--- src/components/View.vue
+++ src/components/View.vue
@@ -4,25 +4,84 @@ import SvgIcon from '@/components/global/SvgIcon.vue'
 import { useStore } from '@/stores/store'
 
 const store = useStore()
-const ratio = ref(40)
-// const filter = ref('')
+const ratio = ref(45)
 const activeTab = ref('one')
 
 const tabs = [
-	{ id: 0, modified: false, name: 'one', label: 'Справочник' },
+	{ id: 0, modified: false, name: 'one', label: 'Атрибуты' },
 	{ id: 1, modified: true, name: 'two', label: 'Узел' },
 	{ id: 2, modified: false, name: 'three', label: 'Представления' },
 ]
 const paket = ref([
 	{
-		label: 'One',
+		id: 0,
+		label: 'Контракт',
 		header: 'root',
-		children: [{ label: 'Two' }, { label: 'trhee' }, { label: 'foru' }],
+		children: [
+			{
+				id: 1,
+				label: 'Исполнитель',
+				icon: 'mdi-folder-outline',
+				children: [
+					{
+						id: 5,
+						label: 'Выписка из ЕГРЮЛ',
+						icon: 'mdi-file-document-outline',
+					},
+					{
+						id: 6,
+						label: 'Устав предприятия',
+						icon: 'mdi-file-document-outline',
+					},
+					{
+						id: 7,
+						label: 'Учредительные документы',
+						icon: 'mdi-file-document-outline',
+					},
+					{
+						id: 8,
+						label: 'Доверенность на представителя директора',
+						icon: 'mdi-file-document-outline',
+					},
+					{
+						id: 9,
+						label: 'Декларации за прошедший год',
+						icon: 'mdi-file-document-outline',
+					},
+					{
+						id: 10,
+						label: 'Документы о соответствии тендеру',
+						icon: 'mdi-file-document-outline',
+					},
+					{
+						id: 11,
+						label: 'Сертификат банковской гарантии',
+						icon: 'mdi-file-document-outline',
+					},
+				],
+			},
+			{
+				id: 2,
+				label: 'Документы об оплате',
+				icon: 'mdi-folder-outline',
+			},
+			{
+				id: 3,
+				label: 'Исполнение контракта',
+				icon: 'mdi-file-document-outline',
+			},
+			{
+				id: 4,
+				label: 'Информация о заказчике',
+				icon: 'mdi-file-document-outline',
+			},
+		],
 	},
 ])
 const add = () => {
 	store.toggleRightDrawer()
 }
+const selected = ref(1)
 </script>
 
 <template lang="pug">
@@ -34,11 +93,19 @@ q-page(padding)
 			template(#before)
 				q-scroll-area.home.pr
 					q-btn(round icon="mdi-plus" size="md" dark color="primary" @click="add").fab
-					q-tree(:nodes="paket" node-key="label" default-expand-all icon="mdi-chevron-right")
+					q-tree(:nodes="paket"
+						node-key="id"
+						default-expand-all
+						v-model:selected="selected"
+						icon="mdi-chevron-right")
 						template(v-slot:header-root="prop")
 							.row.items-center
-								svg-icon( name="packet").q-mr-sm
+								component(:is="SvgIcon" name="packet" size="30px").q-mr-sm
 								div {{ prop.node.label }}
+						template(v-slot:default-header="prop")
+							.row.items-center
+								q-icon(:name="prop.node.icon" size="24px").q-mr-sm
+								span {{ prop.node.label }}
 					
 			template(#after)
 				q-tabs(v-model='activeTab' dense align="left").text-primary
@@ -49,6 +116,8 @@ q-page(padding)
 </template>
 
 <style scoped lang="scss">
+@import '@/assets/styles/myvariables.scss';
+
 .home {
 	height: calc(100vh - 170px);
 	position: relative;
@@ -82,5 +151,15 @@ q-page(padding)
 }
 :deep(.q-tree__arrow) {
 	font-size: 1.3rem;
+}
+.q-tree:deep(.q-icon) {
+	color: $primary;
+}
+:deep(.q-tree__node--selected) {
+	background: #b1ddfc;
+	color: $primary;
+}
+:deep(.q-tree__node--selected .q-tree__node-header-content) {
+	color: $primary;
 }
 </style>
