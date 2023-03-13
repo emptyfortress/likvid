6dfb0c902ea6043aefa298810d4f8f336d5fbf25
Volkov Alexander <volkov.525@gmail.com>
2023-03-13 10:24:46 +0300

    remove comments

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,9 @@
-<script setup lang="ts">
-// import SvgIcon from '@/components/global/SvgIcon.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template lang="pug">
 q-page(padding)
 	.container
 		h4 home
-		// SvgIcon(name="test" color="var:info" hover-color)
-		// SvgIcon(name="packet")
 </template>
 
 <style scoped lang="scss"></style>
