99c1e74dfa1d71016b4724c39aa419e863959c36
Volkov Alexander <volkov.525@gmail.com>
2023-03-21 14:21:25 +0300

    add filter

--- src/components/Folders.vue
+++ src/components/Folders.vue
@@ -25,6 +25,9 @@ const calcTo = computed(() => {
 	return '/'
 })
 const selected = ref([])
+const clear = () => {
+	selected.value = []
+}
 
 const select = (e: any) => {
 	router.push('/contract')
@@ -40,7 +43,7 @@ function selectCode(tree: any, id: number) {
 </script>
 
 <template lang="pug">
-q-page(padding)
+q-page.column.justify-between
 	.container
 		.row.items-baseline.justify-between.q-gutter-x-lg
 			.zag {{ item?.title }}
@@ -51,17 +54,42 @@ q-page(padding)
 			flat
 			color="primary"
 			:rows="rows"
+			:filter="store.filter"
 			rows-per-page-label="Показать на странице"
 			:rows-per-page-options=[10,20,50,0]
 			row-key="id"
-      selection="multiple"
+      selection="single"
 			no-data-label="Данные отсутствуют."
       v-model:selected="selected"
 			).q-mt-md
-
+	.total(v-show="selected.length")
+		.row.items-center.q-gutter-x-md
+			q-btn(flat round dense icon="mdi-close" @click="clear")
+			.tot {{selected.length}}
+			.arr &rarr;
+			q-btn(unelevated color="primary" label="Создать на основе" to="/contract")
 </template>
 
 <style scoped lang="scss">
+.container {
+	width: 100%;
+	max-width: 1200px;
+	margin: 0 auto;
+}
+.total {
+	height: 50px;
+	background: #e4e4e0;
+	width: 100%;
+	max-width: 1168px;
+	margin: 0 auto;
+	padding: 0.5rem;
+	border-radius: 1rem 1rem 0 0;
+	border-top: 1px solid #ccc;
+	border-left: 1px solid #ccc;
+	border-right: 1px solid #ccc;
+	box-shadow: 0 -2px 6px rgba(0, 0, 0, 0.12);
+}
+
 .create {
 	margin-top: 1rem;
 }
